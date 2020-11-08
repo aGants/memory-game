5a76c722cc43d3739e95f7e169833e36ecc7af4a
Adelia <adelgants@gmail.com>
2020-11-08 20:06:41 +0300

    feat: add dataset

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,24 @@
 const cards = document.querySelectorAll('.memory-card');
 
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+cards.forEach(card => card.addEventListener('click', flipCard))
+
 function flipCard() {
   this.classList.toggle('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    console.log(firstCard.dataset.name);
+    console.log(secondCard.dataset.name);
+
+  }
+  
 }
-cards.forEach(card => card.addEventListener('click', flipCard))
