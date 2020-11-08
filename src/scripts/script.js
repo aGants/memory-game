85d58762b7c2638bb90fdf473a10815aa8ecbb4c
Adelia <adelgants@gmail.com>
2020-11-08 20:16:58 +0300

    feat: add check for match

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,17 +8,32 @@ cards.forEach(card => card.addEventListener('click', flipCard))
 
 function flipCard() {
   this.classList.toggle('flip');
-
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
   } else {
     hasFlippedCard = false;
     secondCard = this;
-
-    console.log(firstCard.dataset.name);
-    console.log(secondCard.dataset.name);
-
+    checkForMatch()
   }
-  
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.name === secondCard.dataset.name) {
+    disableCards()
+  } else {
+    unflipCards()
+  }
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, (1000));
 }
